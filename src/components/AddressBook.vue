d1a032a79705b7b4aa403505b968e1b5aac6f046
Adam Barney <adambarney2@gmail.com>
2020-11-25 13:10:06 -0500

    Add address page

---
+++ src/components/AddressBook.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Enter an address here</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddressBook'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/views/Address.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <AddressBook></AddressBook>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AddressBook from '@/components/AddressBook.vue'
+
+export default {
+  name: 'Address',
+  components: {
+    AddressBook
+  }
+}
+</script>
